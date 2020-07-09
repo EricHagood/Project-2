88c31939dca5f18458f4b88da0eb6ba8be4f26f8
Eric  Hagood <eric@Erics-MacBook-Pro.local>
2020-07-09 16:07:41 -0500

    Changed Schema structure for character data and fleshed out new and edit routes

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,27 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 
 const Character = require("../models/characters.js");
-const testCharacter = {
-    name: "Alexander",
-    background: "Outlander",
-    characterClass: [{
-        characterClass1: "Fighter", 
-        level: 4
-    },{
-        characterClass2: "Paladin", 
-        level: 2
-    }],
-    backStory: "He has one.",
-    items: ["Glaive", "Plate Armor", "Holy Symbol"]
-}
-// Character.create(testCharacter, (err, character)=>{
-//     if (err){console.log(err);}
-//     else{console.log(character)}
-// });
-console.log(Character);
+
+
 //Index route, displays all of the characters in the database
 router.get('/', (req, res)=>{
     Character.find({}, (error, characters)=>{
@@ -30,6 +15,21 @@ router.get('/', (req, res)=>{
         })
     })
 })
+
+//Test data
+router.get('/seed', (req, res)=>{
+    Character.create([{
+        name: "Alexander",
+        race: "Variant Human",
+        background: "Outlander",
+        characterClass: ["Fighter", "Paladin"],
+        level: ['4', '2'],
+        backStory: "He has one.",
+        items: ["Glaive", "Plate Armor", "Holy Symbol"],
+        isDead: false
+    }])
+})
+
 //edit route 
 router.get('/:id/edit', (req, res)=>{
     Character.findById(req.params.id, (err, foundCharater)=>{
@@ -39,24 +39,32 @@ router.get('/:id/edit', (req, res)=>{
     })
 })
 
+//New route
 router.get('/new', (req, res)=>{
     res.render('new.ejs')
 })
 
 
+//Post route to make new entries
 router.post('/', (req, res)=>{
+    console.log(req.body);
+    console.log(req.body.backStory);
     if(req.body.isDead === 'on'){
         req.body.idDead = true;
     }else{
         req.body.isDead = false;
     }
     Character.create(req.body);
+    res.redirect('/characters');
 })
 
 
+//Put route to update entries
 router.put('/:id', (req, res)=>{
-    if (req.body.isDead == 'on'){
-        req.body.idDead = true;
+    console.log(req.body);
+    console.log(req.body.name);
+    if (req.body.isDead === 'on'){
+        req.body.isDead = true;
     }else{
         req.body.isDead = false;
     }
@@ -74,24 +82,8 @@ router.get('/:id', (req, res)=>{
     })
 })
 
-//Test data
-router.get('/seed', (req, res)=>{
-    Character.create([{
-        name: "Alexander",
-        background: "Outlander",
-        characterClass: [{
-            characterClass: "Fighter", 
-            level: 4
-        },{
-            characterClass: "Paladin", 
-            level: 2
-        }],
-        backStory: "He has one.",
-        items: ["Glaive", "Plate Armor", "Holy Symbol"],
-        isDead: false
-    }])
-})
 
+//Delete route to delete characters from database
 router.delete('/:id', (req, res)=>{
     Character.findByIdAndRemove(req.params.id, {useFindAndModify: false}, (err, data)=>{
         res.redirect('/characters')
--- models/characters.js
+++ models/characters.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const characterSchema = new mongoose.Schema({
     name: {type: String, required: true},
+    race: {type: String, required: true},
     background: {type: String, required: true},
-    characterClass: {type: Array, required: true},
+    characterClass: {type: Array},
+    level: {type: Array},
     backStory: {type: String},
     items: {type: Array},
     isDead: {type: Boolean}
